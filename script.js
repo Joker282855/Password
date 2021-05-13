ec9ae2415e2453e3b3f48afa6918d6ffdbdfe09c
Joshua <jtjackson325@gmail.com>
2021-05-13 15:18:51 -0600

    added variables for uppercase, lowercase, number, and special

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characterSelect = document.querySelector("password");
+var upperCase = document.querySelector("password");
+var lowerCase = docmunet.querySelector("password");
+var specialCase = document.querySelector("password");
 
 // Write password to the #password input
 function writePassword () {
-  window.prompt("How many charaters would you like your password to contain");
-  window.confirm("Click Ok to confirm including uppercase letters");
+  window.prompt("How many characters would you like your password to contain");
   window.confirm("Click OK to confirm including lowercase letters");
   window.confirm("Click Ok to confirm including numeric values");
   window.confirm("Click OK to confirm including special charaters");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -16,4 +20,3 @@ function writePassword () {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
