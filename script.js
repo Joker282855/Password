a691fc20c3e21b5c0983b64faa7d15f52d007f4b
Joshua <jtjackson325@gmail.com>
2021-05-13 12:11:46 -0600

    added all of the prompts to the password generator

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
-function writePassword() {
+function writePassword () {
+  window.prompt("How many charaters would you like your password to contain");
+  window.confirm("Click Ok to confirm including uppercase letters");
+  window.confirm("Click OK to confirm including lowercase letters");
+  window.confirm("Click Ok to confirm including numeric values");
+  window.confirm("Click OK to confirm including special charaters");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
+
