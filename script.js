1e178518c4b35325d26f0c7ee0399f65001a2580
Joshua <jtjackson325@gmail.com>
2021-05-15 17:31:53 -0600

    made it so user has to pick a number between 8 and 128 for the length of their password

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function writePassword () {
   var characterSelect = window.prompt("How many characters do you want in your password");
   if (characterSelect < 8 || characterSelect > 128) {
     window.prompt("Must pick a number between 8-128");
+    writePassword();
   } else {
     characterSelect = Math.random(Math.floor () * 9) + 120;
   }
@@ -13,47 +14,45 @@ function writePassword () {
   for (var i=0; i < upperCase.length; i++) {
     if (upperCase > 0) {
       upperCase = Math.random(Math.floor() *upperCase.length);
-
-      return value;
     }
     else {
       upperCase = upperCase[i] * 0;
     }
+    return upperCase;
   }
   var lowerCase = window.confirm("Click OK to confirm including uppercase letters");
   for (var i=0; i < lowerCase.length; i++) {
     if (lowerCase > 0) {
-      lowerCase = Math.floor(Math.random() * lowerCase.length);
-
-      return vlaue;
+      lowerCase = Math.floor(Math.random() * lowerCase.length);  
     }
     else {
       lowerCase = lowerCase[i] * 0;
     }
+    return lowerCase;
   }
   var numberInput = window.confirm("Click OK to confirm including numeric values");
   for (var i=0; i < numberInput.length; i++) {
     if (numberInput > 0) {
       numberInput = Math.floor(Math.random() * numberInput.length);
 
-      return value;
+      
     } else {
       numberInput = numberInput[i] * 0;
     }
+    return numberInput;
   }
   var specialCase = window.confirm("Click OK to confirm including special charaters");
   for (var i=0; i < specialCase.length; i++) {
     if (specialCase > 0) {
-      specialCase = Math.floor(Math.random() * specialCase.length);
-      
-      return value;
+      specialCase = Math.floor(Math.random() * specialCase.length);  
     }
     else {
-      specialCase = specialCase[i] * 0;
-
-      
+      specialCase = specialCase[i] * 0;  
     }
+    return specialCase;
   }  
+  
+  window.alert(" Your password is " + upperCase + lowerCase + numberInput + specialCase);
   
   characterSelect = Math.random(Math.floor() * 9) + 120;
   upperCase = ["A", "B", "C"];
@@ -61,7 +60,6 @@ function writePassword () {
   numberInput = [5, 6, 7];
   specialCase = ["@", "$", "%"];
 
-  window.alert(" Your password is " + characterSelect + upperCase + lowerCase + numberInput + specialCase);
   var password = generatePassword(characterSelect, upperCase, lowerCase, numberInput, specialCase);
   var passwordText = document.querySelector("#password");  
 
