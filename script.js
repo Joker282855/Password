b3db02bf31134d4d50f2dd5236f340a21ee34e50
Joshua <jtjackson325@gmail.com>
2021-05-16 00:04:06 -0600

    created another branch and cleaned up some code to make sure the prompts were working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword () {
+function writePassword (passwordText) {
   var characterSelect = window.prompt("How many characters do you want in your password");
   if (characterSelect < 8 || characterSelect > 128) {
     window.prompt("Must pick a number between 8-128");
@@ -11,56 +11,36 @@ function writePassword () {
     characterSelect = Math.random(Math.floor () * 9) + 120;
   }
   var upperCase = window.confirm("Click OK to confirm including lowercase letters");
-  for (var i=0; i < upperCase.length; i++) {
-    if (upperCase > 0) {
-      upperCase = Math.random(Math.floor() *upperCase.length);
-    }
+  if (upperCase === "OK") {
+    upperCase = "SDFHGEISOKDHEOLE";
+  } 
     else {
-      upperCase = upperCase[i] * 0;
-    }
-    return upperCase;
+    upperCase = "12345679890329";
   }
   var lowerCase = window.confirm("Click OK to confirm including uppercase letters");
-  for (var i=0; i < lowerCase.length; i++) {
-    if (lowerCase > 0) {
-      lowerCase = Math.floor(Math.random() * lowerCase.length);  
-    }
+  if (lowerCase === "OK") {
+    lowerCase = "thyuadksiorlsenl";  
+  }
     else {
-      lowerCase = lowerCase[i] * 0;
-    }
-    return lowerCase;
+    lowerCase = "@$#%^&@!**";
   }
   var numberInput = window.confirm("Click OK to confirm including numeric values");
-  for (var i=0; i < numberInput.length; i++) {
-    if (numberInput > 0) {
-      numberInput = Math.floor(Math.random() * numberInput.length);
-
-      
-    } else {
-      numberInput = numberInput[i] * 0;
-    }
-    return numberInput;
+  if (numberInput === "OK") {
+    numberInput = "236758490657";
+  } else {
+    numberInput = "GHRTYUISJGIER";
   }
   var specialCase = window.confirm("Click OK to confirm including special charaters");
-  for (var i=0; i < specialCase.length; i++) {
-    if (specialCase > 0) {
-      specialCase = Math.floor(Math.random() * specialCase.length);  
-    }
+  if (specialCase === "OK") {
+    specialCase = "#$@^%&*$*!@#$";  
+  }
     else {
-      specialCase = specialCase[i] * 0;  
-    }
-    return specialCase;
-  }  
-  
-  window.alert(" Your password is " + upperCase + lowerCase + numberInput + specialCase);
-  
-  characterSelect = Math.random(Math.floor() * 9) + 120;
-  upperCase = ["A", "B", "C"];
-  lowerCase = ["a", "b", "c"];
-  numberInput = [5, 6, 7];
-  specialCase = ["@", "$", "%"];
+    specialCase = "thesdfwr";  
+  }
 
-  var password = generatePassword(characterSelect, upperCase, lowerCase, numberInput, specialCase);
+  
+  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");  
 
   passwordText.value = password;
